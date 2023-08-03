f8667ed947891fe8362fc602aa2623f2f92a8758
Murali C <murc@publicisgroupe.net>
2023-08-03 13:51:53 +0530

    AddedComments

--- src/main/java/com/murali/ecomercesite/Test/LoginTestCRM.java
+++ src/main/java/com/murali/ecomercesite/Test/LoginTestCRM.java
@@ -19,6 +19,7 @@ public class LoginTestCRM extends BaseTest {
         loginPage=new LoginPage(driver);
         loginPage.ClickLoginButton();
         logger.pass("Login Page is loaded, is passed");
+        System.out.println("Login Page is loaded, is passed");
         loginPage.logintoCRM( excelDataProvider.getStringData("Login",1,0), excelDataProvider.getStringData("Login",1,1));
         logger.pass("Login is successfull with excel data");
 
--- src/main/java/com/murali/ecomercesite/pageObjects/freeCRM/BaseTest.java
+++ src/main/java/com/murali/ecomercesite/pageObjects/freeCRM/BaseTest.java
@@ -20,51 +20,51 @@ public class BaseTest {
     public WebDriver driver;
     public ExcelDataProvider excelDataProvider;
     public configDataProvider conf;
-    public  ExtentTest logger;
+    public ExtentTest logger;
     public ExtentReports report;
 
 
     @BeforeSuite
-    public void suiteLoading(){
-        Reporter.log("Suite Config is getting loaded",true);
-        excelDataProvider=new ExcelDataProvider();
-        conf=new configDataProvider();
-        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(new File(System.getProperty("user.dir")+"./Reports/FreeCRM_"+Helper.getCurrentDateTime()+".html"));
+    public void suiteLoading() {
+        Reporter.log("Suite Config is getting loaded", true);
+        excelDataProvider = new ExcelDataProvider();
+        conf = new configDataProvider();
+        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(new File(System.getProperty("user.dir") + "./Reports/FreeCRM_" + Helper.getCurrentDateTime() + ".html"));
         report = new ExtentReports();
         report.attachReporter(htmlReporter);
-        Reporter.log("Suite Config is loaded completely.. Test is getting started hold tight",true);
+        Reporter.log("Suite Config is loaded completely.. Test is getting started hold tight", true);
     }
 
-//    @BeforeMethod
+    //    @BeforeMethod
 //    public void beforeMethodSetup(){
 //        test = extent.createTest(getClass().getSimpleName());
 //    }
-    @Parameters({"browser","url"})
+    @Parameters({"browser", "url"})
     @BeforeClass
-    public void setup(String browser,String appurl){
-        Reporter.log("Browser and App url is getting loaded",true);
-       // driver= browserFactory.startApp(driver,conf.getBrowser(),conf.getappURl());
-        driver= browserFactory.startApp(driver,browser,appurl);
-        Reporter.log("Browser and App url is loaded successfully",true);
+    public void setup(String browser, String appurl) {
+        Reporter.log("Browser and App url is getting loaded", true);
+        // driver= browserFactory.startApp(driver,conf.getBrowser(),conf.getappURl());
+        driver = browserFactory.startApp(driver, browser, appurl);
+        Reporter.log("Browser and App url is loaded successfully", true);
 
     }
 
     @AfterClass
-    public void teardown(){
-        Reporter.log("Closing the browser!!",true);
+    public void teardown() {
+        Reporter.log("Closing the browser!!", true);
         browserFactory.quitBrowser(driver);
-        Reporter.log("Closed the browser!!",true);
+        Reporter.log("Closed the browser!!", true);
     }
 
     @AfterMethod
     public void tearDownAfterMethod(ITestResult iTestResult) throws IOException {
         Reporter.log("Getting your reported based on test Results", true);
-        if(iTestResult.getStatus()==ITestResult.FAILURE) {
+        if (iTestResult.getStatus() == ITestResult.FAILURE) {
             logger.fail("Test Failed", MediaEntityBuilder.createScreenCaptureFromPath(Helper.getScreenShot(driver)).build());
-        }else if(iTestResult.getStatus()==ITestResult.SUCCESS){
+        } else if (iTestResult.getStatus() == ITestResult.SUCCESS) {
             logger.pass("Test Passed", MediaEntityBuilder.createScreenCaptureFromPath(Helper.getScreenShot(driver)).build());
 
-        }else if(iTestResult.getStatus()==ITestResult.SKIP){
+        } else if (iTestResult.getStatus() == ITestResult.SKIP) {
             logger.warning("Test Skipped", MediaEntityBuilder.createScreenCaptureFromPath(Helper.getScreenShot(driver)).build());
         }
         Reporter.log("Report is ready .. check and feel good", true);
